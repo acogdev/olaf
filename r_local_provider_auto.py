01970dfe713f9f60497e5c14eaeb7935ad4dc3e1
acogdev <jake@acogdev.com>
2016-10-11 16:01:31 -0600

    more work on login

--- r_local_provider_auto.py
+++ r_local_provider_auto.py
@@ -4,17 +4,19 @@ from requests_oauthlib import OAuth2Session
 import olaf_lib
 
 
+r = requests.post('http://127.0.0.1:5000/token',
+                  data={'username': 'jake',
+                        'password': 'jake'})
+id_token = r.text
+
 r = requests.post('http://127.0.0.1:5000',
-                  data={'username': 'jake'})
+                  data={'username': 'jake'},
+                  headers={'Authorization': id_token})
 
 if r.status_code == 200:
-    r = requests.post('http://127.0.0.1:5000/token',
-                      cookies=r.cookies,
-                      data={'username': 'jake', 'password': 'jake'})
-
     r1 = requests.get('http://127.0.0.1:5000/client',
                       cookies=r.cookies,
-                      headers={'Authorization': r.text})
+                      headers={'Authorization': id_token})
 
     if r1.status_code == 200:
         client_dat = r1.json()
@@ -30,7 +32,6 @@ if r.status_code == 200:
             f.write('\n')
 
         authorization_base_url = 'http://127.0.0.1:5000/oauth/authorize'
-
 
         SESSION = OAuth2Session(client_id,
                                 redirect_uri=olaf_lib.getRedirectURI(),
